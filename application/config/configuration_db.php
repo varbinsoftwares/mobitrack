f5945078ad2f470d5cd71e0d5253a6a93446e20c
octopuscart <dev@develop.com>
2021-09-16 01:51:08 +0530

    chagnes

--- application/config/configuration_db.php
+++ application/config/configuration_db.php
@@ -1,5 +1,22 @@
 <?php
+
 $activedb = "mobitracker_app";
 $activeusername = "mobitracker_app";
 $activepassword = "JE&6gGOcaRR[";
+
+$baselink = 'http://' . $_SERVER['SERVER_NAME'];
+
+if (strpos($baselink, 'varbin')) {
+
+    $activedb = "j2k5e6r5_araskocon";
+    $activeusername = "j2k5e6r5_octopus";
+    $activepassword = "India$2017";
+}
+
+if (strpos($baselink, 'localhost')) {
+
+    $activedb = "j2k5e6r5_araskocon";
+    $activeusername = "j2k5e6r5_octopus";
+    $activepassword = "India$2017";
+}
 ?>
